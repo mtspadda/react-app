57d631191cdf34dfd3bb70fec5a5e8696f3d2631
Matheus Marques <matheuspadilha.dev@gmail.com>
2025-08-13 15:56:33 +0100

    handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import ListGroup from './components/ListGroup';
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["TOP", "MID", "JUNGLE", "BOT", "SUPORT"];
 
-  return <div><ListGroup/> </div>
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading={"Positions"}
+        onSelectItem={handleSelectItem}
+      />{" "}
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,40 @@
-function ListGroup() {
-  const items = ["top", "mid", "jungle", "bot", "suport"];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  const getMessage = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
+
   items.map((item) => <li key={item}>{item}</li>);
   return (
     <>
-      <h1> List </h1>
+      <h1> {heading} </h1>
+      {getMessage()}
+      {items.length === 0 && <p> No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
